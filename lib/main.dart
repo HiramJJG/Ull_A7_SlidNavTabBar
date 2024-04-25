0b3bb415dff9de442e7a8b9d214e671af3c97405
Hiram_Jurado <hiramjurado16@gmail.com>
2024-04-25 18:49:59 +0000

    a7

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "Flutter Demo Home Page"),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://raw.githubusercontent.com/HiramJJG/img_IOS/main/BalBasquet.jpg",
+    "https://raw.githubusercontent.com/HiramJJG/img_IOS/main/Balon%20UCL.jpeg",
+    "https://raw.githubusercontent.com/HiramJJG/img_IOS/main/Chamarra%20Nike.jpeg",
+    "https://raw.githubusercontent.com/HiramJJG/img_IOS/main/Air%20Max%20Excee%20White.jpeg",
+    "https://raw.githubusercontent.com/HiramJJG/img_IOS/main/Adidas%20Forum.jpeg",
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imgList
+                  .map((e) => Center(
+                        child: Center(
+                          child: Image.network(e),
+                        ),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  initialPage: 0,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  onPageChanged: (value, _) {
+                    setState(() {
+                      _currentPage = value;
+                    });
+                  }),
+            ),
+            buildCarouselIndicator()
+          ],
         ),
       ),
+    );
+  }
+
+  buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < imgList.length; i++)
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: i == _currentPage ? 7 : 5,
+            width: i == _currentPage ? 7 : 5,
+            decoration: BoxDecoration(
+                color: i == _currentPage ? Colors.black : Colors.red,
+                shape: BoxShape.circle),
+          )
+      ],
     );
   }
 }
